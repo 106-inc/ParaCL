13b40dad39bd6e7252f622f848c36b1519f6c120
uslsteen <shurygin0171@gmail.com>
2021-02-15 16:54:00 +0300

    creating new class and main.cc

---
+++ main.cc
@@ -0,0 +1,14 @@
+#include "driver.hh"
+
+
+int main(int argc, char** argv)
+{
+    if (argc < 2)
+    {
+        std::cout << "Lack of arguments!\n";
+        return 0;
+    }
+
+    yy::Driver driver(argv[1]);
+    driver.parse();
+}
