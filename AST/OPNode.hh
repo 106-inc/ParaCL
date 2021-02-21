16fe3af8fa6608f9a0f5688fd9e2504391dd236a
andrey <derzhavin.andrej@yandex.ru>
2021-02-22 00:35:17 +0300

    added simple arithmetic operators nodes

--- AST/OPNode.hh
+++ AST/OPNode.hh
@@ -16,6 +16,30 @@ namespace AST
 
     int calc() const override;
   };
+
+  class SBNode final : public OPNode
+  {
+  public:
+    SBNode( INode *left, INode *right );
+
+    int calc() const override;
+  };
+
+  class MLNode final : public OPNode
+  {
+  public:
+    MLNode( INode *left, INode *right );
+
+    int calc() const override;
+  };
+
+  class DVNode final : public OPNode
+  {
+  public:
+    DVNode( INode *left, INode *right );
+
+    int calc() const override;
+  };
 }
 
 #endif //PARACL_OPNODE_HH
