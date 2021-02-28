5fd876d21604c3b17bc37683552e01a87cc2c297
uslsteen <shurygin0171@gmail.com>
2021-03-01 00:27:05 +0300

    fix location

--- parser/parser.cc
+++ parser/parser.cc
@@ -4,17 +4,18 @@
 
 #include "parser.hh"
 
-yy::location OurFlexLexer::get_cur_location()
+yy::location OurFlexLexer::get_cur_location() { return cur_location_; }
+
+std::string OurFlexLexer::get_cur_str_() { return cur_str_; }
+
+void OurFlexLexer::upd_cur_loc()
 {
-  return cur_location_;
+    int cur_num_of_line = lineno();
+
+    cur_location_.lines(cur_num_of_line - last_num_of_line);
+    cur_location_.columns((YYLeng()));
+    cur_location_.step();
+
+    last_num_of_line = cur_num_of_line;
 }
 
-std::string OurFlexLexer::get_cur_str_()
-{
-  return cur_str_;
-}
-
-void upd_cur_loc()
-{
-    //lineno(); ??????????????
-}
