9e7e3e6201f9357408040907867a9baa5680b7b9
andrey <derzhavin.andrej@yandex.ru>
2021-03-01 15:21:28 +0300

    1) added comments 2) renamed to struct 3) deleted deleted ctor and assgn op 4) moved functions realizations into .hh

--- AST/OPNode.hh
+++ AST/OPNode.hh
@@ -9,52 +9,104 @@
 
 namespace AST
 {
-class PLNode final : public OPNode
+/**
+ * @brief Plus node struct
+ */
+struct PLNode final : public OPNode
 {
-public:
-  PLNode(INode *left, INode *right);
+  /**
+   * @brief PLus node struct ctor
+   * @param left left node ptr
+   * @param right right node ptr
+   */
+  PLNode(INode *left, INode *right) : OPNode(left, right)
+  {
+  }
 
-  PLNode(const PLNode &) = delete;
-  PLNode &operator=(const PLNode &) = delete;
-
-  int calc() const override;
-};
-
-class SBNode final : public OPNode
-{
-public:
-  SBNode(INode *left, INode *right);
-
-  SBNode(const SBNode &) = delete;
-  SBNode &operator=(const SBNode &) = delete;
-
-  int calc() const override;
-};
-
-class MLNode final : public OPNode
-{
-public:
-  MLNode(INode *left, INode *right);
-
-  MLNode(const MLNode &) = delete;
-  MLNode &operator=(const MLNode &) = delete;
-
-  int calc() const override;
-};
-
-class DVNode final : public OPNode
-{
-public:
-  DVNode(INode *left, INode *right);
-
-  DVNode(const DVNode &) = delete;
-  DVNode &operator=(const DVNode &) = delete;
-
-  int calc() const override;
+  /**
+   * @brief Calculate value of plus node function
+   * @return calculated sum value
+   */
+  int calc() const override
+  {
+    return left_->calc() + right_->calc();
+  } /* End of 'calc' function */
 };
 
 /**
- * @brief Assignment operator's node
+ * @brief Sub node struct
+ */
+struct SBNode final : public OPNode
+{
+  /**
+   * @brief Sub node struct ctor
+   * @param left left node ptr
+   * @param right right node ptr
+   */
+  SBNode(INode *left, INode *right) : OPNode(left, right)
+  {
+  }
+
+  /**
+   * @brief Calculate value of subtract node function
+   * @return calculated subtract value
+   */
+  int calc() const override
+  {
+    return left_->calc() - right_->calc();
+  } /* End of 'calc' function */
+};
+
+/**
+ * @brief Mul node struct
+ */
+struct MLNode final : public OPNode
+{
+  /**
+   * @brief Mul node struct ctor
+   * @param left left node ptr
+   * @param right right node ptr
+   */
+  MLNode(INode *left, INode *right) : OPNode(left, right)
+  {
+  }
+
+  /**
+   * @brief Calculate value of multiply node function
+   * @return calculated product value
+   */
+  int calc() const override
+  {
+    return left_->calc() * right_->calc();
+  } /* End of 'calc' function */
+};
+
+/**
+ * @brief Division node struct
+ */
+struct DVNode final : public OPNode
+{
+  /**
+   * @brief Division node struct ctor
+   * @param left left node ptr
+   * @param right right node ptr
+   */
+  DVNode(INode *left, INode *right) : OPNode(left, right)
+  {
+  }
+
+  /**
+   * @brief Calculate value of division node function
+   * @return calculated divided value
+   */
+  int calc() const override
+  {
+    return left_->calc() / right_->calc();
+  } /* End of 'calc' function */
+};
+
+/**
+ * @brief Assignment operator's node class
  */
 class ASNode final : public INode
 {
@@ -62,105 +114,228 @@ private:
   VNode *dst_;  // variable to assign
   INode *expr_; // expression
 public:
-  ASNode(VNode *dst, INode *expr);
+  /**
+   * @brief Assignment node class ctor
+   * @param dst pointer to destination variable node
+   * @param expr pointer to expression node(-s)
+   */
+  ASNode(VNode *dst, INode *expr) : dst_(dst), expr_(expr)
+  {
+  }
 
-  ASNode(const ASNode &) = delete;
-  ASNode &operator=(const ASNode &) = delete;
+  /**
+   * @brief Calculate an assignment function
+   * @return calculated assigned value
+   */
+  int calc() const override
+  {
+    int expr_res = expr_->calc();
 
-  int calc() const override;
+    dst_->set_val(expr_res);
 
-  ~ASNode();
-};
-// TODO:
-// ADD Logical operators classes
-/////////////////////////
-//
-class EQNode final : public OPNode
-{
-public:
-  EQNode(INode *left, INode *right);
+    return expr_res;
+  } /* End of 'calc' function */
 
-  EQNode(const EQNode &) = delete;
-  EQNode &operator=(const EQNode &) = delete;
-
-  int calc() const override;
-};
-
-class NEQNode final : public OPNode
-{
-public:
-  NEQNode(INode *left, INode *right);
-
-  NEQNode(const NEQNode &) = delete;
-  NEQNode &operator=(const NEQNode &) = delete;
-
-  int calc() const override;
+  /**
+   * @brief Assignment class dtor
+   */
+  ~ASNode()
+  {
+    delete dst_;
+    delete expr_;
+  } /* End of '~ASNode' function */
 };
 
-class GNode final : public OPNode
+/**
+ * @brief Equality node struct
+ */
+struct EQNode final : public OPNode
 {
-public:
-  GNode(INode *left, INode *right);
+  /**
+   * @brief Equality node struct ctor
+   * @param left left node ptr
+   * @param right right node ptr
+   */
+  EQNode(INode *left, INode *right) : OPNode(left, right)
+  {
+  }
 
-  GNode(const GNode &) = delete;
-  GNode &operator=(const GNode &) = delete;
-
-  int calc() const override;
+  /**
+   * @brief Calculate value of equality node function
+   * @return calculated is equality value
+   */
+  int calc() const override
+  {
+    return left_->calc() == right_->calc();
+  } /* End of 'calc' function */
 };
 
-class GENode final : public OPNode
+/**
+ * Not equality node struct
+ */
+struct NEQNode final : public OPNode
 {
-public:
-  GENode(INode *left, INode *right);
+  /**
+   * @brief Not equality node struct ctor
+   * @param left left node ptr
+   * @param right right node ptr
+   */
+  NEQNode(INode *left, INode *right) : OPNode(left, right)
+  {
+  }
 
-  GENode(const GENode &) = delete;
-  GENode &operator=(const GENode &) = delete;
-
-  int calc() const override;
+  /**
+   * @brief Calculate value of not equality node function
+   * @return calculated not equality checking value
+   */
+  int calc() const override
+  {
+    return left_->calc() != right_->calc();
+  } /* End of 'calc' function */
 };
 
-class LNode final : public OPNode
+/**
+ * @brief Greater node struct
+ */
+struct GNode final : public OPNode
 {
-public:
-  LNode(INode *left, INode *right);
+  /**
+   * @brief Greater node struct ctor
+   * @param left left node ptr
+   * @param right right node ptr
+   */
+  GNode(INode *left, INode *right) : OPNode(left, right)
+  {
+  }
 
-  LNode(const LNode &) = delete;
-  LNode &operator=(const LNode &) = delete;
-
-  int calc() const override;
+  /**
+   * @brief Calculate value of greater node function
+   * @return calculated value
+   */
+  int calc() const override
+  {
+    return left_->calc() > right_->calc();
+  } /* End of 'calc' function */
 };
 
-class LENode final : public OPNode
+/**
+ * @brief Greater-or-equal node struct
+ */
+struct GENode final : public OPNode
 {
-public:
-  LENode(INode *left, INode *right);
+  /**
+   * @brief Greater-or-equal node struct ctor
+   * @param left left node ptr
+   * @param right right node ptr
+   */
+  GENode(INode *left, INode *right) : OPNode(left, right)
+  {
+  }
 
-  LENode(const LENode &) = delete;
-  LENode &operator=(const LENode &) = delete;
-
-  int calc() const override;
+  /**
+   * @brief Calculate value of Greater-or-equal node function
+   * @return calculated value
+   */
+  int calc() const override
+  {
+    return left_->calc() >= right_->calc();
+  } /* End of 'calc' function */
 };
 
-class ANDNode final : public OPNode
+/**
+ * @brief Less node struct
+ */
+struct LNode final : public OPNode
 {
-public:
-  ANDNode(INode *left, INode *right);
+  /**
+   * @brief Less node struct ctor
+   * @param left left node ptr
+   * @param right right node ptr
+   */
+  LNode(INode *left, INode *right) : OPNode(left, right)
+  {
+  }
 
-  ANDNode(const ANDNode &) = delete;
-  ANDNode &operator=(const ANDNode &) = delete;
-
-  int calc() const override;
+  /**
+   * @brief Calculate value of less node function
+   * @return calculated value
+   */
+  int calc() const override
+  {
+    return left_->calc() < right_->calc();
+  } /* End of 'calc' function */
 };
 
-class ORNode final : public OPNode
+/**
+ * @brief Less-or-equal node struct
+ */
+struct LENode final : public OPNode
 {
-public:
-  ORNode(INode *left, INode *right);
+  /**
+   * @brief Less-or-equal node struct ctor
+   * @param left left node ptr
+   * @param right right node ptr
+   */
+  LENode(INode *left, INode *right) : OPNode(left, right)
+  {
+  }
 
-  ORNode(const ORNode &) = delete;
-  ORNode &operator=(const ORNode &) = delete;
+  /**
+   * @brief Calculate value of Less-or-equal node function
+   * @return calculated value
+   */
+  int calc() const override
+  {
+    return left_->calc() <= right_->calc();
+  } /* End of 'calc' function */
+};
 
-  int calc() const override;
+/**
+ * @brief Logical AND node struct
+ */
+struct ANDNode final : public OPNode
+{
+  /**
+   * @brief Logical AND node struct ctor
+   * @param left left node ptr
+   * @param right right node ptr
+   */
+  ANDNode(INode *left, INode *right) : OPNode(left, right)
+  {
+  }
+
+  /**
+   * @brief Calculate value of logical AND node function
+   * @return calculated conjuncted value
+   */
+  int calc() const override
+  {
+    return left_->calc() && right_->calc();
+  } /* End of 'calc' function */
+};
+
+/**
+ * @brief Logical OR node struct
+ */
+struct ORNode final : public OPNode
+{
+  /**
+   * @brief Logical OR node struct ctor
+   * @param left left node ptr
+   * @param right right node ptr
+   */
+  ORNode(INode *left, INode *right) : OPNode(left, right)
+  {
+  }
+
+  /**
+   * @brief Calculate value of Logical OR node function
+   * @return calculated disjuncted value
+   */
+  int calc() const override
+  {
+    return left_->calc() || right_->calc();
+  } /* End of 'calc' function */
 };
 
 } // namespace AST
