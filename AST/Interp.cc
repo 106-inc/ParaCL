9def5da2fb0f9322e49177a5095243531a85a807
derzhavin3016 <derzhavin.andrej@yandex.ru>
2021-04-04 04:28:59 +0300

    interpreter cycle added

--- AST/Interp.cc
+++ AST/Interp.cc
@@ -13,16 +13,43 @@ void Interp::interpret()
 {
   FrameStack.emplace(globl_, 0);
 
-  while (FrameStack.top().state != static_cast<int>(States::END))
+  while (!FrameStack.empty())
   {
-    auto pNode = FrameStack.top().node;
+    auto &fr_top = FrameStack.top();   // fr_top == (global, 0)
 
-    size_t chld_size = pNode->get_ch_size();
+    // get the current node ptr
+    pINode pNode = fr_top.node;   // == global 
+    
+    size_t childs_am = pNode->get_ch_size(); // 2
+    // get child of current node 
+    int i_next = fr_top.state; // 0
 
-    for (size_t i = 0; i < chld_size; ++i)
+    if (i_next == static_cast<int>(States::END))
     {
-      
+      fr_top.node->calc();
+      FrameStack.pop();
+      continue;
     }
+    if (i_next < 0)
+      throw std::runtime_error{"Negative child index"};
+
+    // go to next state
+    fr_top.state++;
+
+    if (fr_top.state >= childs_am)
+      fr_top.state = static_cast<int>(States::END);
+
+    auto pch = pNode->get_i_child(i_next); // 
+    if (pch == nullptr)
+    {
+      fr_top.state = static_cast<int>(States::END);
+      continue;
+    }
+    FrameStack.emplace(pch, 0);
+
+    if (pch->get_ch_size() == 0)
+      FrameStack.top().state = static_cast<int>(States::END);
+
   }
 } /* End of 'interpret' function */
 } // namespace AST
